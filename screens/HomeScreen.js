85d5aee823b1ff2d325828ad937f7f44ee571982
Ev Fitz Dixon <efitzdixon201@outlook.com>
2023-11-25 08:13:58 -0600

    Navigation Part 4

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,53 @@
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
+import { useState } from 'react';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotion';
+import { PARTNERS } from '../shared/partners';
+
+
+const FeaturedItem = ({ item }) => {
+    if (item) {
+        return (
+            <Card containerStyle={{ padding: 0 }}>
+                <Card.Image source={item.image}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                            }}
+                        >
+                            {item.name}
+                        </Text>
+                    </View>
+                </Card.Image>
+                <Text style={{ margin: 20 }}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+};
 
 const HomeScreen = () => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+    const [promotions, setPromotions] = useState(PROMOTIONS);
+    const [partners, setPartners] = useState(PARTNERS);
+
+    const featCampsite = campsites.find((item) => item.featured);
+    const featPromotion = promotions.find((item) => item.featured);
+    const featPartner = partners.find((item) => item.featured);
+
     return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <ScrollView>
+            <FeaturedItem item={featCampsite} />
+            <FeaturedItem item={featPromotion} />
+            <FeaturedItem item={featPartner} />
+        </ScrollView>
     );
 };
 
 export default HomeScreen;
-
---
+++ shared/promotion.js
@@ -0,0 +1,10 @@
+export const PROMOTIONS = [
+    {
+        id: 0,
+        name: 'Mountain Adventure',
+        image: require("../assets/images/breadcrumb-trail.jpg"),
+        featured: true,
+        description:
+            'Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equipment and lessons provided.',
+    },
+];
